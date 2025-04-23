b0cbbac50c589d5651609e4a1e43dd85caf107f5
EstebanCaponio <estebancaponio@gmail.com>
2025-04-23 17:46:33 +0200

    feat: refactoring

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,14 +1,10 @@
 import PostsList from "../components/PostsList";
-import { useList } from "../contexts/ListContext";
 
 export default function ListaDeiPost() {
-
-    // const { posts } = useList();
 
     return (
         <>
             <PostsList />
-            {/* {posts} */}
         </>
     )
 }
